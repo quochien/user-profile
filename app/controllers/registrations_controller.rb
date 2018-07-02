9b4a1d16cf047f6ca706f32baf2a43d15f57ca30
Hien Nguyen <quochien@gmail.com>
2018-07-02 19:01:53 +0700

    user can edit name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def user_root_path
     profile_users_path
   end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
