f90b4d8ee97ae5180a6090d9ea5a0cef53932e61
Hien Nguyen <quochien@gmail.com>
2018-07-02 18:48:43 +0700

    rating for expert: reviews list

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -5,5 +5,30 @@ class ExpertsController < ApplicationController
 
   def show
     @expert = User.find_by_id(params[:id])
+    calculate_ratings
+
+    if params[:filter] == "written"
+      @reviews = @expert.reviews.where.not(comments: [nil, ""])
+    else
+      @reviews = @expert.reviews
+    end
+  end
+
+  private
+
+  def calculate_ratings
+    ratings = @expert.reviews.group(:rating).count
+    total_reviews = ratings.values.sum
+
+    @rating_counts = {}
+    (1..5).each do |star|
+      @rating_counts[star] = {}
+      @rating_counts[star][:star] = ratings[star.to_f].to_i
+      if total_reviews == 0
+        @rating_counts[star][:percent] = 0
+      else
+        @rating_counts[star][:percent] = (ratings[star.to_f].to_f * 100 / total_reviews).round
+      end
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def display_date(date)
+    days = (Date.today - date.to_date).to_i
+
+    case days
+    when 0
+      "Today"
+    when 1
+      "Yesterday"
+    else
+      "#{days} days ago"
+    end
+  end
 end
