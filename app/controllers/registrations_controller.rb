357af1e71592870753790e1d1733df588733c6be
Hien Nguyen <quochien@gmail.com>
2018-07-03 20:43:15 +0700

    fix signup user name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,8 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
